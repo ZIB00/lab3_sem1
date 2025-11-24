2be5388f192f42a5497880927d8b3087d030d279
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2025-11-24 16:05:29 +0300

    Модель списка

--- main/logic.c
+++ main/logic.c
@@ -9,5 +9,10 @@ struct Node{
 
 int Queue() {
     struct Node * list = NULL;
+    Node a = {3}, b = {17}, c = {21}, t = {10};
+    list = &a;
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
     return 0;
 }
