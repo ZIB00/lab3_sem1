3d6787cbbe04cae07f5679fb2df26a36de2ed415
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2025-11-24 15:50:45 +0300

    Модель list

---
+++ main/logic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct Node Node;
+
+struct Node{
+    int data;
+    Node * next;
+};
+
+int Queue() {
+    struct Node * list = NULL;
+    return 0;
+}
---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include "logic.h"
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    printf("ypa");
+    return 0;
+}
