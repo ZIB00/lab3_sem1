d1dbd0bd9495eeab37d5e101fc498453af5928d4
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2025-11-25 22:55:16 +0300

    Добавлены enqueue, is_empty, изменена тестовая ф-ция, удалены старые аналоги нофых ф-ций

--- main/queue.c
+++ main/queue.c
@@ -19,7 +19,7 @@ void print(Queue * q) {
     printf("\n"); 
 }
 
-void enqueue1(Queue * q, int data) {
+void enqueue(Queue * q, int data) {
     Node * new_Node = malloc(sizeof(Node));
     new_Node->data = data;
     new_Node->next = NULL;
@@ -32,60 +32,49 @@ void enqueue1(Queue * q, int data) {
         q->tail = new_Node;
     }
 }
-
-
-void enqueue(Node ** phead, Node ** ptail, int d) {
-    Node * p = malloc(sizeof(Node));
-    p->data = d;
-    p->next = NULL;
-
-    if (*phead == NULL) {
-        *phead = p;
-        *ptail = p;
+int is_empty(Queue * q) {
+    if (q->head == NULL) {
+        return 1;
     } else {
-        (*ptail)->next = p;
-        *ptail = p;
+        return 0;
     }
 }
+int dequeue(Queue * q, int * r) {
+    if (!is_empty(q)) {
+        Node * old_head = q->head;
+        *r = q->head->data;
+        q->head = q->head->next;
 
-int dequeue(Node ** phead, Node ** ptail) {
-    Node * p = *phead;
-    int res = p->data;
+        if (q->head == NULL) {
+            q->tail = NULL;
+        }
 
-    *phead = p->next;
-
-    if (*phead == NULL) {
-        *ptail = NULL;
+        free(old_head);
+        return 1;
     }
-
-    free(p);
-    return res;
-}
-
-int is_empty(Node * list) {
-    return list == NULL;
+    return 0;
 }
 
 int queue() {
     int test[] = {21, 17, 3, 10};
-    Node * head = NULL;
-    Node * tail = NULL;
+    int res, *r = res;
+    Queue * q = {NULL, NULL};
 
-    printf("Empty: %s\n", is_empty(head) ? "YES" : "NO");
+    printf("Empty: %s\n", is_empty(q) ? "YES" : "NO");
 
     for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); i++) {
-        enqueue(&head, &tail, test[i]);
-        print(head);
+        enqueue(&q, test[i]);
+        print(q);
     }
 
-    printf("Empty: %s\n", is_empty(head) ? "YES" : "NO");
+    printf("Empty: %s\n", is_empty(q) ? "YES" : "NO");
 
-    while (!is_empty(head)) {
-        int d = dequeue(&head, &tail);
+    while (!is_empty(q)) {
+        int d = dequeue(q, &r);
         printf("pop %d :", d);
-        print(head);
+        print(q);
     }
 
-    printf("Empty: %s\n", is_empty(head) ? "YES" : "NO");
+    printf("Empty: %s\n", is_empty(q) ? "YES" : "NO");
     return 0;
 }
